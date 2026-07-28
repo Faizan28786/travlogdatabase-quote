e54177a768dce0d9b1ac0185df9767bdc2134896
Faizan Ansari <write2faizan.28@gmail.com>
2026-07-28 23:49:40 +0530

    Fix Gmail SMTP IPv4

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -3,16 +3,24 @@ const {
     quotationEmailTemplate
 } = require("../templates/quotationEmail");
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4,
+
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 transporter.verify(function (err, success) {
     if (err) {
