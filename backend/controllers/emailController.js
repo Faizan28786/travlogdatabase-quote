eac494346cbebdf09bf081531c5a2d293cd974e4
Faizan Ansari <write2faizan.28@gmail.com>
2026-07-28 23:26:45 +0530

    SMTP timeout fix

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -4,14 +4,21 @@ const {
 } = require("../templates/quotationEmail");
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    tls: {
-        rejectUnauthorized: false
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000
+});
+transporter.verify(function (err, success) {
+    if (err) {
+        console.log("SMTP ERROR:", err);
+    } else {
+        console.log("SMTP READY");
     }
 });
 
