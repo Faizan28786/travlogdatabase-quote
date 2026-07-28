39c710050701697c50b26f3aa471bb6cd1570154
Faizan Ansari <write2faizan.28@gmail.com>
2026-07-29 00:51:10 +0530

    af

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -1,45 +1,41 @@
+const nodemailer = require("nodemailer");
+const {
+    quotationEmailTemplate
+} = require("../templates/quotationEmail");
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,          // <-- IMPORTANT
-
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-
-    tls: {
-        rejectUnauthorized: false
-    }
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000
 });
-
-console.log("EMAIL USER:", process.env.EMAIL_USER);
-console.log("EMAIL PASS EXISTS:", !!process.env.EMAIL_PASS);
 
 exports.sendQuoteEmail = async (req, res) => {
     try {
 
-        const { to, subject, html } = req.body;
+        const {
+            to,
+            subject,
+            html
+        } = req.body;
 
-        if (!to) {
+        if (!to)
             return res.status(400).json({
                 success: false,
                 message: "Recipient Email Missing"
             });
-        }
 
-        console.log("Sending mail to:", to);
-
-        const info = await transporter.sendMail({
+        await transporter.sendMail({
             from: `"TravLog" <${process.env.EMAIL_USER}>`,
             to,
             subject,
             html: quotationEmailTemplate(html)
         });
-
-        console.log("MAIL SENT SUCCESS");
-        console.log(info.response);
 
         res.json({
             success: true,
@@ -48,7 +44,7 @@ exports.sendQuoteEmail = async (req, res) => {
 
     } catch (err) {
 
-        console.error("MAIL ERROR FULL:", err);
+        console.log(err);
 
         res.status(500).json({
             success: false,
