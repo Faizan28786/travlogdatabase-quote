ff31d467e1a8f0d6daf52856eef06294e91b036e
Faizan Ansari <write2faizan.28@gmail.com>
2026-07-28 23:19:18 +0530

    Fixed Gmail SMTP configuration

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -3,10 +3,15 @@ const {
     quotationEmailTemplate
 } = require("../templates/quotationEmail");
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
