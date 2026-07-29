59fb5144cf5eb3d928d9f1e0a50e1dc1e211e3b0
Faizan Ansari <write2faizan.28@gmail.com>
2026-07-29 10:57:10 +0530

    Fix email controller and SMTP configuration

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -6,20 +6,10 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    requireTLS: true,
 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-
-    tls: {
-        family: 4,
-        rejectUnauthorized: false
     }
 });
 exports.sendQuoteEmail = async (req, res) => {
